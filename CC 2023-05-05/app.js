59ee6e9ac52c8d155b4d8372d0e6d428028d9b6c
Sarah Schilz <sarahschilz88@gmail.com>
2023-05-06 04:21:09 -0700

    Added edgecases from 5-5

---
+++ CC 2023-05-05/app.js
@@ -0,0 +1,24 @@
+// Given an array of integers, return a new array 
+//with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// P: will it always be taking in integers? Are we going
+// to be given any special characters? An empty arr ever?
+// R: Return a new arr with each value doubled
+// E: If we are given [2, 3, 4], should return [4, 6, 8]
+//    If we are given [4, 5, 6], should return [8, 10, 12]
+//    If we are given [2, 22], should return [4, 44]
+// P: 
+// Make a function that takes in an array
+function doubled(arr){
+    if(arr === undefined || arr.length === 0){
+        console.log('Array is invalid')
+    }else{
+        return arr.map(element=>element*2)
+    }
+}
+doubled([1, 2, 3])
+// map through the array and multiply each element by 2 and return
