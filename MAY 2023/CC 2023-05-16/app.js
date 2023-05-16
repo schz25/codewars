27adcac75dc60422e03af38bcfa3d2cd30059a5e
Sarah Schilz <sarahschilz88@gmail.com>
2023-05-16 12:36:30 -0700

    Solution for coding challenge on 2023-05-16 PALIDROME USING ARROW FUNCTION

--- MAY 2023/CC 2023-05-16/app.js
+++ MAY 2023/CC 2023-05-16/app.js
@@ -2,12 +2,14 @@
 //str -> letters, all lower, spec, funny biz
 //return true or false
 
-function isPalidrome(word){
-    //split, reverse, join
-    //rev === word
-    let reversedWord = word.split('').reverse().join('')
-    return reversedWord === word
-}
+// function isPalidrome(word){
+//     //split, reverse, join
+//     //rev === word
+//     let reversedWord = word.split('').reverse().join('')
+//     return reversedWord === word
+// }
+
+const isPalidrome = word => word === word.split('').reverse().join('')
 
 console.log(isPalidrome('racecar'), 'true')
 console.log(isPalidrome('leonnoel'), 'true')
