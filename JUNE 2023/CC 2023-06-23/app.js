105a1c5909146ca69fae739ff6b20af734221020
Sarah Schilz <sarahschilz88@gmail.com>
2023-06-22 13:35:54 -0700

    Solution for coding challenge on 2023-06-22

--- JUNE 2023/CC 2023-06-23/app.js
+++ JUNE 2023/CC 2023-06-23/app.js
@@ -4,8 +4,21 @@
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
 
+// function reverseWords(str) {
+//     // Go for it
+//     let revStr = str.split("").reverse().join("");
+//     return revStr;
+//   }
+
+  //using map
 function reverseWords(str) {
-    // Go for it
-    let revStr = str.split("").reverse().join("");
-    return revStr;
-  }
+    return str.split(' ').map(function(word){
+        return word.split('').reverse().join('');
+    }).join(' ');
+}
+
+// anotha one
+
+function reverseWords(str) {
+    return str.split('').reverse().join('').split(' ').reverse().join(' ');
+}
