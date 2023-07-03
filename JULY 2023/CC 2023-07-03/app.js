e9302e0a4da05771f17023f5e81b7a246216e7c7
Sarah Schilz <sarahschilz88@gmail.com>
2023-07-03 13:51:24 -0700

    Solution for coding challenge on 2023-07-03

---
+++ JULY 2023/CC 2023-07-03/app.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+function square(num){
+    return num ** 2;
+}
+
+// using big arrow
+
+const square = (n) => n * n;
