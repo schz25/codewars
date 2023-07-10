d96e5dc5110f0266d12dd1ba916e1bcaa726eb11
Sarah Schilz <sarahschilz88@gmail.com>
2023-07-10 13:47:01 -0700

    Solution for coding challenge on 2023-07-10

---
+++ JULY 2023/CC 2023-07-10/app.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//P: taking in 2 strings
+
+function solution(str, ending) {
+    //TODO: complete
+    return str.endsWith(ending);
+}
+
+// string.endsWith(searchValue, length)
