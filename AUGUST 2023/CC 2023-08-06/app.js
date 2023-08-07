a20a5012aa690b7acf968999e9e745dc427f28f1
Sarah Schilz <sarahschilz88@gmail.com>
2023-08-06 18:50:36 -0700

    Solution for coding challenge on 2023-08-06

---
+++ AUGUST 2023/CC 2023-08-06/app.js
@@ -0,0 +1,22 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    //code here
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+}
+
+// another solution
+const XO = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+}
