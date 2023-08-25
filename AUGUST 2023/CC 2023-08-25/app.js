f91227144bd509ffa516ae2bc85049f5efa8aff7
Sarah Schilz <sarahschilz88@gmail.com>
2023-08-25 15:17:36 -0700

    Solution for coding challenge on 2023-08-25

---
+++ AUGUST 2023/CC 2023-08-25/app.js
@@ -0,0 +1,32 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+// best practice
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+        return (max - min + 1) * (min + max) / 2;
+}
+
+// another solution
+function GetSum(a,b){
+    return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+// another solution
+
+function GetSum(a,b){
+    if(a == b) return a;
+    else if (a < b) return a + GetSum(a+1, b);
+    else return a + GetSum(a-1,b);
+}
