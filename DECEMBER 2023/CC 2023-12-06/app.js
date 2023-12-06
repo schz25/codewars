d4b33999fa1caa4732e8a2de068c4db80e75c21c
Sarah Schilz <sarahschilz88@gmail.com>
2023-12-06 11:44:25 -0800

    Solution to coding challenge on 2023-12-06

---
+++ DECEMBER 2023/CC 2023-12-06/app.js
@@ -0,0 +1,39 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// Solution using arrow function
+
+switchItUp = n => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][n]
+
+
+// Solution using switch statement
+
+function switchItUp2(number){
+    switch(number){
+        case 0:
+            return 'Zero';
+        case 1:
+            return 'One';
+        case 2:
+            return 'Two';
+        case 3:
+            return 'Three';
+        case 4:
+            return 'Four';  
+        case 5:
+            return 'Five'; 
+        case 6:
+            return 'Six';    
+        case 7:
+            return 'Seven'; 
+        case 8:
+            return 'Eight';    
+        case 9:
+            return 'Nine';
+        default:
+            return 'Unkown number'; 
+    }
+}
