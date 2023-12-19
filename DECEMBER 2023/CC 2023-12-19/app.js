20ddd55e17fd552c789adf5492ce73f9c2049c5e
Sarah Schilz <sarahschilz88@gmail.com>
2023-12-19 13:36:50 -0800

    Solution for coding challenge on 2023-12-19

---
+++ DECEMBER 2023/CC 2023-12-19/app.js
@@ -0,0 +1,13 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+String.prototype.isUpperCase = function() {
+    return this.valueOf().toUpperCase() === this.valueOf();
+}
