f33110cedc428663feac04ada608ba7e40bce8e0
Sarah Schilz <sarahschilz88@gmail.com>
2023-12-20 13:55:29 -0800

    Solution to coding challenge on 2023-12-20

---
+++ DECEMBER 2023/CC 2023-12-20/app.js
@@ -0,0 +1,24 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// filter creates a copy of the array and checks if the index is equal to 0 after dividing by 2 which would be every second elements and returns it
+function removeEveryOther(arr){
+    return arr.filter(function(elem, index){
+        return index % 2 === 0;
+    })
+}
+
+// another solution
+
+
+function removeEveryOther2(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i += 2){
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
