d1b4458613cf6b8e47909396edf002e13b45e728
Sarah Schilz <sarahschilz88@gmail.com>
2024-01-04 10:49:48 -0800

    Solution for coding challenge on 2024-01-04

---
+++ JANUARY 2024/CC 2024-01-04/app.js
@@ -0,0 +1,13 @@
+// Given an integer or a floating-point number, find its opposite
+
+// Example
+
+// 1 : -1
+// 14: -14
+// -34: 34
+
+// Solution
+
+function opposite(number){
+    return(-number);
+}
