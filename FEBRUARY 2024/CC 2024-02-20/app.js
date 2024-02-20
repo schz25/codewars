2c08cc3b3e7460fde0c4f02f129d127935dbc71a
Sarah Schilz <sarahschilz88@gmail.com>
2024-02-20 14:37:34 -0800

    Solution for coding challenge 2023-02-20

--- FEBRUARY 2024/CC 2024-02-20/app.js
+++ FEBRUARY 2024/CC 2024-02-20/app.js
@@ -19,3 +19,19 @@ const rps = (p1, p2) => {
     else
         return 'Player 2 won!'
 }
+
+//another solution
+
+const rockpaperscissors = (p1, p2) => {
+    if(p1 == p2)
+        return "Draw!"
+
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+
+    if(p2 === rules[p1]){
+        return "Player 1 won!"
+    }
+    else{
+        return "Player 2 won!"
+    }
+}
