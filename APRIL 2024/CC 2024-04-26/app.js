15e5d605a42c347cfa603bccea86d2a792e37be9
Sarah Schilz <sarahschilz88@gmail.com>
2024-04-26 10:19:35 -0700

    Solution for coding challenge on 2024-04-26

---
+++ APRIL 2024/CC 2024-04-26/app.js
@@ -0,0 +1,24 @@
+// Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// multiples(3, 5.0)
+// should return
+
+// [5.0, 10.0, 15.0]
+
+//returns new array
+// 3 => 5.0, 10.0, 15.0 first 3 multiples of 5.0
+// i = 0, i <= m, i++
+// n * n push
+
+
+function multiples(m, n){
+    const result = [];
+
+    for(i = 1; i <= m; i++){
+        result.push(n * i);
+    }
+
+    return result;
+}
